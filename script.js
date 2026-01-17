cc83ab13e507c6260bd9afc0961ca290fdbf58fb
HaroldRoot <ngoksausi618@gmail.com>
2026-01-18 06:59:29 +0800

    :children_crossing: Add tips, update style

--- script.js
+++ script.js
@@ -999,9 +999,7 @@ document.addEventListener("DOMContentLoaded", () => {
             '\u0352', '\u0357', '\u0351', '\u0307', '\u0308', '\u030a', '\u0342', '\u0343',
             '\u0344', '\u034a', '\u034b', '\u034c', '\u0303', '\u0302', '\u030c', '\u0350',
             '\u0300', '\u0301', '\u030b', '\u030f', '\u0312', '\u0313', '\u0314', '\u033d',
-            '\u0309', '\u0363', '\u0364', '\u0365', '\u0366', '\u0367', '\u0368', '\u0369',
-            '\u036a', '\u036b', '\u036c', '\u036d', '\u036e', '\u036f', '\u033e', '\u035b',
-            '\u0346', '\u031a'
+            '\u0309', '\u033e', '\u035b', '\u0346', '\u031a'
         ],
         mid: [
             '\u0315', '\u031b', '\u0340', '\u0341', '\u0358', '\u0321', '\u0322', '\u0334',
@@ -1475,6 +1473,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 else brailleArtString = brailleArtString + '\n' + brailleArt;
                 dotsOutputContainer.innerHTML = "";
                 const card = createResultCard(brailleArtString);
+                card.className = 'font-result-card dots-result-card';
                 dotsOutputContainer.appendChild(card);
             }
         });
@@ -1778,7 +1777,7 @@ document.addEventListener("DOMContentLoaded", () => {
     initKaomoji();
 
     const url = document.getElementById("url-display");
-    
+
     url.addEventListener('click', () => {
         copyToClipboard(url.textContent);
     });
